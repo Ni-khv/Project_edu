1388e6572174f7bf00d90d61a00716079a053316
Okeyancyc <ivasenetanikita@gmail.com>
2024-04-16 22:47:37 +0200

    first commit

---
+++ src/JS/bobik.js
@@ -0,0 +1,19 @@
+const arr = [1,2,3];
+
+console.log(arr[1]);
+
+const arrObj = {
+	a:1,
+	b:2,
+	c:3
+
+};
+console.log(arrObj['c']);
+
+
+const answers =[];
+answers[0] = prompt['Как тебя зовут?', ''];
+answers[1] = prompt['Какая твоя фамилия?', ''];
+answers[2] = prompt['Сколько тебе лет?', ''];
+
+console.log(typeof(answers));
