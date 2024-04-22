d8ebb3e3161d5516a3d40ede96216b5a850dcd58
Okeyancyc <ivasenetanikita@gmail.com>
2024-04-22 23:01:22 +0200

    add new code in bobik

--- src/JS/bobik.js
+++ src/JS/bobik.js
@@ -17,3 +17,19 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+const num = 50;
+switch (num) {
+case 49:
+	console.log('Неверно');
+	break;
+case 100:
+	console.log('Много');
+	break;
+case 50:
+	console.log('Ok!');
+	break;
+default:
+	console.log('Не в этот раз');
+	break;
+}
