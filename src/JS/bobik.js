b4b84b974bbb49f795494a7bb409cbb6f92ff863
Okeyancyc <ivasenetanikita@gmail.com>
2024-05-01 22:19:24 +0200

    закоментировал бобика

--- src/JS/bobik.js
+++ src/JS/bobik.js
@@ -1,22 +1,22 @@
-const nubersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const nubersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-	count: nubersOfFilms,
-	movies: {},
-	actors:{},
-	genres:[],
-	privat: false 
-};
+// const personalMovieDB = {
+// 	count: nubersOfFilms,
+// 	movies: {},
+// 	actors:{},
+// 	genres:[],
+// 	privat: false 
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('Насколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('Насколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 	b = prompt('Насколько оцените его?', ''),
+// 	c = prompt('Один из последних просмотренных фильмов?', ''),
+// 	d = prompt('Насколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 const num = 50;
 switch (num) {
