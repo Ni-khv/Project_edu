b072f931977976de054594fe714548ff76ceda95
Okeyancyc <ivasenetanikita@gmail.com>
2024-05-06 23:45:59 +0200

    new exerce

--- src/JS/bobik.js
+++ src/JS/bobik.js
@@ -1,35 +1,46 @@
-// const nubersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const nubersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-// 	count: nubersOfFilms,
-// 	movies: {},
-// 	actors:{},
-// 	genres:[],
-// 	privat: false 
-// };
+const personalMovieDB = {
+	count: nubersOfFilms,
+	movies: {},
+	actors:{},
+	genres:[],
+	privat: false 
+};
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-// 	b = prompt('Насколько оцените его?', ''),
-// 	c = prompt('Один из последних просмотренных фильмов?', ''),
-// 	d = prompt('Насколько оцените его?', '');
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+for(let i=0; i < 2; i++){
+	const a = prompt('Один из последних просмотренных фильмов?', ''), 
+		b = prompt('Насколько оцените его?', '');
+	if(a !=null && b !== null && a !==''&& b !=='' &&a.length<50 && b.length<50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else{
+		console.log('Error');
+		i--;
+	}
 
-// console.log(personalMovieDB);
-
-const num = 50;
-switch (num) {
-case 49:
-	console.log('Неверно');
-	break;
-case 100:
-	console.log('Много');
-	break;
-case 50:
-	console.log('Ok!');
-	break;
-default:
-	console.log('Не в этот раз');
-	break;
+	
+	personalMovieDB.movies[a] = b;
 }
+
+
+console.log(personalMovieDB);
+
+// const num = 50;
+// switch (num) {
+// case 49:
+// 	console.log('Неверно');
+// 	break;
+// case 100:
+// 	console.log('Много');
+// 	break;
+// case 50:
+// 	console.log('Ok!');
+// 	break;
+// default:
+// 	console.log('Не в этот раз');
+// 	break;
+// }
+
+
